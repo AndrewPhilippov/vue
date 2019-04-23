ccf1196fba99b6f625a05832fc641311c3e5a805
Andrew <a@fandb.pro>
2019-04-23 22:14:23 +0300

    Exercise complete

--- ex2/app.js
+++ ex2/app.js
@@ -2,5 +2,13 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
+        },
+        methods: {
+            alert: function(){
+                alert("Hello!")
+            },
+            getValue: function(e){
+                this.value = e.target.value;
+            }
         }
     });
