b57180fd0d229d08960ecb63616acac411629c34
Andrew <a@fandb.pro>
2019-04-27 16:01:13 +0300

    Completed v-for, v-if, v-else topics

---
+++ 10/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#app",
+    data: {
+        ingredients: ["meat", "vegetables", "fruits", "fish"],
+        persons: [
+            {name: "Andrew", age: 32, color: "blue"},
+            {name: "Lia", age: 32, color: "purple"},
+            {name: "Delia", age: 5, color: "red"},
+            {name: "Aaron", age: 3, color: "green"},
+        ]
+    }
+})
