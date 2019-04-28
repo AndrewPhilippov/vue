8f5041d9017a448e203edcd6659cdf329095ff69
Andrew <a@fandb.pro>
2019-04-28 12:48:10 +0300

    Completed first Vue project - now you can fight with monster and heal yourself if he was rude.

--- monster-game-app/app.js
+++ monster-game-app/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function(){
@@ -12,21 +13,53 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function(){
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            let damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster for ${damage}`
+            });
             if(this.checkWin()){
                 return;
             }
-             this.playerHealth -= this.calculateDamage(5, 10);
-             this.checkWin();
+            this.monsterAttack();
         },
         specialAttack: function(){
-
+            let damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster HARD for ${damage}`
+            });
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttack();
         },
         heal: function(){
-
+            if(this.playerHealth <= 90){
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player heals for 10`
+            });
+            this.monsterAttack();
         },
         giveUp: function(){
-
+            this.gameIsRunning = false;
+            this.turns = [];
+        },
+        monsterAttack: function(){
+            let damage = this.calculateDamage(5, 10);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage}`
+            });
+            this.checkWin();
         },
         calculateDamage(min, max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
