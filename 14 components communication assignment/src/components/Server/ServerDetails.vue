ce8d4b90d19b2acd59a1bee1dc429324f2a7ce06
Andrew <a@fandb.pro>
2019-05-03 15:11:23 +0300

    diving in more advanced components

---
+++ 14 components communication assignment/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select a server</p>
+        <p v-else>Server #{{ server.id }} selected. Server status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to normal</button>
+    </div>
+
+</template>
+
+<script>
+    import { serverBus } from "../../main";
+
+    export default {
+        data: function(){
+            return {
+                server: null
+            }
+        },
+        created(){
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server
+            })
+        },
+        methods: {
+            resetStatus(){
+                this.server.status = 'Normal'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
