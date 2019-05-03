36dcac8b77fc8522d3ad7dc45b5b58b9d8d3c54e
Andrew <a@fandb.pro>
2019-05-03 16:46:20 +0300

    Completed dynamic components and slots section

---
+++ 15 components continue/src/components/New.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>New Quote: </h3>
+        <button @click="counter++">{{ counter }}</button>
+    </div>
+</template>
+<script>
+    export default {
+        data: function(){
+            return {
+                counter: 0
+            }
+        },
+        destroyed() {
+            console.log('destrooooo-ooo-o-o-o-ooo-o-o-oo-o-o-o-o--o-o-o-o--ooo-o-o-oo-yed!!!')
+        },
+        deactivated(){
+            console.log('Deactivated');
+        },
+        activated() {
+            console.log('It\'s alive!!!')
+        }
+    }
+</script>
+<style scoped>
+</style>
