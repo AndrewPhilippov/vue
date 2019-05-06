516555fdd3798dece25e0c642aa00f4b44c8bd0e
Andrew <a@fandb.pro>
2019-05-06 07:08:29 +0300

    Completed wonderful quotes tutorial and moving to form control section

--- 17 quotes-app/src/App.vue
+++ 17 quotes-app/src/App.vue
@@ -1,12 +1,45 @@
 <template>
     <div class="container">
-    
+        <app-header :quote-count="quotes.length" :max-quotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <div class="alert alert-info">Info: click on a quote to delete it.</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue'
+    import Header from './components/Header.vue'
     export default {
-        
+        data(){
+            return{
+                quotes: [
+                    'Just an initial quote to start with smth...'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods:{
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete quote to add new one...')
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1)
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
     }
 </script>
 
