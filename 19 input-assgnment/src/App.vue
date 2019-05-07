b993ae0bee57063142a154f1a022172506b45f6e
Andrew <a@fandb.pro>
2019-05-07 22:54:06 +0300

    Completed assignment on input section

--- 19 input-assgnment/src/App.vue
+++ 19 input-assgnment/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -9,10 +9,29 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label for="">Mail</label>
+                        <input type="email" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" name="" id="" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" name="" id="" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <button type="submit" @click.prevent="submitted = true" class="btn btn-primary">Submit</button>
 
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
@@ -21,17 +40,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +59,20 @@
 </template>
 
 <script>
+    import FullName from './components/FullName.vue'
     export default {
+        data(){
+            return {
+                fullName: 'Andrew Philippov',
+                email: '',
+                password: '',
+                storeData: 'Yes',
+                submitted: false
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
     }
 </script>
 
