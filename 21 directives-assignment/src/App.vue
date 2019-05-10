0a51304d5ca6d0ad679f78856e888243d8f7fa28
Andrew <a@fandb.pro>
2019-05-10 22:22:37 +0300

    Completed diectives assignment

--- 21 directives-assignment/src/App.vue
+++ 21 directives-assignment/src/App.vue
@@ -5,7 +5,11 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
+                <hr>
+                <div style="width:100px; height: 100px; background-color: lightgoldenrodyellow;"
+                    v-custom-on:mouseenter="mouseEnter"
+                    v-custom-on:mouseleave="mouseLeave"></div>
             </div>
         </div>
     </div>
@@ -14,7 +18,27 @@
 <script>
     export default {
         directives: {
-
+            customOn: {
+                bind(el, binding){
+                    // el.onclick = function(){
+                    //     binding.value();
+                    // }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn)
+                }
+            }
+        },
+        methods: {
+            clicked(){
+                alert('I was clicked!');
+            },
+            mouseEnter(){
+                console.log('The cursor is in');
+            },
+            mouseLeave(){
+                console.log('Mouse left');
+            }
         }
     }
 </script>
