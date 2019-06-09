a6028a11162708aaa77400a09911d152be9cc85d
Andrew <a@fandb.pro>
2019-06-09 12:00:52 +0300

    Fixes for "/user" route

--- 27-vue-router/src/routes.js
+++ 27-vue-router/src/routes.js
@@ -13,14 +13,16 @@ export const routes = [
             'header-top': Header,
         },
     },
-    { path: '/user', components: {
+    { path: '/user',
+        components: {
             default: User,
-            "header-bottom": Header,
+            "header-bottom": Header
+        },
             children: [
             { path: '', component: UserStart },
             { path: ':id', component: UserDetail },
             { path: ':id/edit', component: UserEdit, name: "userEdit" },
-        ] }
+        ]
     },
     { path: '/redirect-me', redirect: { name: "home" } },
     { path: '*', redirect: '/' },
