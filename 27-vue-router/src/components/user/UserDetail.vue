648048258a7e28f9b001f93421a6d2622dbb90e5
Andrew <a@fandb.pro>
2019-06-11 07:58:33 +0300

    Keep on working on routing

--- 27-vue-router/src/components/user/UserDetail.vue
+++ 27-vue-router/src/components/user/UserDetail.vue
@@ -4,7 +4,34 @@
         <p>User loaded has ID: {{ $route.params.id }}</p>
         <router-link
                 tag="button"
-                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
-                class="btn btn-warning">Back to Users page</router-link>
+                :to="link"
+                class="btn btn-warning">Back to Users page
+        </router-link>
     </div>
 </template>
+<script>
+    export default {
+        data(){
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next){
+            if(true){
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+</script>
