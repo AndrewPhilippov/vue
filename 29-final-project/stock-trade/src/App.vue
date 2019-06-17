dffef6f1e96177add98b39144a57ccda67d09da1
Andrew <a@fandb.pro>
2019-06-17 21:43:49 +0300

    Added store and initial vuex setup

--- 29-final-project/stock-trade/src/App.vue
+++ 29-final-project/stock-trade/src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="container">
-      <app-header></app-header>
-      <router-view></router-view>
-  </div>
+    <div class="container">
+        <app-header></app-header>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue';
-  export default {
-    components: {
-      appHeader: Header,
+    import Header from './components/Header.vue';
+
+    export default {
+        components: {
+            appHeader: Header,
+        },
+        created() {
+            this.$store.dispatch('initStocks')
+        }
     }
-  }
 </script>
 
 <style>
-  body{
-    padding: 30px;
-  }
+    body {
+        padding: 30px;
+    }
 </style>
