fdd1f522cec5b830f43f0263a7e2502c3c38f882
a@fandb.pro <a@fandb.pro>
2019-06-30 22:39:51 +0300

    Added interceptors

--- 30-axios-vue/src/main.js
+++ 30-axios-vue/src/main.js
@@ -8,9 +8,22 @@ axios.defaults.baseURL = 'https://vuejs-http-f341c.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'Some value in here';
 axios.defaults.headers.get['Accepts'] = 'application.json';
 
+const reqInterceptor = axios.interceptors.request.use(config => {
+   console.log('Request interceptor: ', config);
+   return config;
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+    console.log('Response interceptor: ', res);
+    return res;
+});
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 });
