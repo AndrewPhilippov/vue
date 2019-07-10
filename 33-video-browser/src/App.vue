5b9f9c197ff318f6a28245205828ad1eb9e3bd2f
a@fandb.pro <a@fandb.pro>
2019-07-10 08:26:32 +0300

    Added axios and connected to youtube API

--- 33-video-browser/src/App.vue
+++ 33-video-browser/src/App.vue
@@ -1,10 +1,31 @@
 <template>
     <div>
-        <h1>Hi there!</h1>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
     </div>
 </template>
 <script>
+    import axios from 'axios'
+    import SearchBar from './components/SearchBar'
+    import VideoList from './components/VideoList'
+    const API_KEY = 'AIzaSyCR-L-g67o14S_pP2tgglM_652mepb2Elg';
     export default {
         name: 'App',
+        components: {
+            SearchBar,
+            VideoList
+        },
+        methods: {
+            onTermChange(searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => console.log(response))
+            }
+        }
     }
 </script>
---
+++ 33-video-browser/src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <label for="">
+            <input type="text" @input="onInput">
+        </label>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput (e) {
+                this.$emit('termChange', e.target.value)
+            }
+        }
+    }
+</script>
+<style></style>
