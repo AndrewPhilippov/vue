9aa0e9c272a172d791135a4160b8b2ab8c1620de
a@fandb.pro <a@fandb.pro>
2019-07-16 07:45:34 +0300

    Initial lesson on Vue w/NetNinja

--- 35-ninja_1/app.js
+++ 35-ninja_1/app.js
@@ -1,6 +1,14 @@
 new Vue({
    el: '#app',
     data: {
-
+        title: 'Becoming Vue ninja',
+        name: 'Andrew',
+        url: 'https://www.youtube.com',
+        classes: ['one', 'two']
+    },
+    methods: {
+       greet(time) {
+           return `Hello and good ${time} ${this.name}`
+       }
     }
 });
