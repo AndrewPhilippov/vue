05c18082a85057d816207768f4e7ef6bfd86c3ed
a@fandb.pro <a@fandb.pro>
2019-07-16 07:56:40 +0300

    Implementing methods

--- 35-ninja_1/app.js
+++ 35-ninja_1/app.js
@@ -1,6 +1,12 @@
 new Vue({
    el: '#app',
     data: {
-
+        title: 'Becoming Vue.js Ninja',
+        wage: 10
+    },
+    methods: {
+        changeWage(amount) {
+            this.wage += amount
+        }
     }
 });
