ffbbaf1ce09162598d09fd7bd021d471c81b9dca
a@fandb.pro <a@fandb.pro>
2019-07-16 08:19:57 +0300

    Events (getting mouse coordinates)

--- 35-ninja_1/app.js
+++ 35-ninja_1/app.js
@@ -2,5 +2,18 @@ new Vue({
    el: '#app',
     data: {
         title: 'Becoming Vue.js Ninja',
+        coords: {
+            x: null,
+            y: null,
+        }
+    },
+    methods: {
+        logEvent(e) {
+            console.log(e);
+        },
+        logCoords(e) {
+            this.coords.x = e.offsetX;
+            this.coords.y = e.offsetY;
+        }
     }
 });
