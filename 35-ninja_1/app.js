b39b93b3260ab72e1b32175083935b9f6bd62b54
a@fandb.pro <a@fandb.pro>
2019-07-16 08:24:34 +0300

    Input keyup/keypress events

--- 35-ninja_1/app.js
+++ 35-ninja_1/app.js
@@ -2,5 +2,11 @@ new Vue({
    el: '#app',
     data: {
         title: 'Becoming Vue.js Ninja',
+        name: 'Andrew'
+    },
+    methods: {
+       updateName(e) {
+           this.name = e.target.value;
+       }
     }
 });
