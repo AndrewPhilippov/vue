87e8730c0b166da3f46cbe0ca8eb0e576bd49890
a@fandb.pro <a@fandb.pro>
2019-07-18 07:06:43 +0300

    Starting Ninja-Smoothies project

---
+++ 36-ninja-smoothies/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+<script>
+  import Navbar from '@/components/Navbar';
+
+  export default {
+    components: {
+      Navbar
+    }
+  };
+</script>
+<style lang="scss">
+  .navbar nav{
+    padding: 0 20px;
+  }
+</style>
---
+++ 36-ninja-smoothies/src/components/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <h2 class="indigo-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ingredient, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ingredient }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      smoothies: [
+        {
+          title: 'Ninja Brew',
+          slug: 'ninja-brew',
+          ingredients: ['bananas', 'coffee', 'milk'],
+          id: 1,
+        },
+        {
+          title: 'Morning Mood',
+          slug: 'morning-mood',
+          ingredients: ['mango', 'lime', 'juice'],
+          id: 2,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter(smoothie => smoothie.id !== id);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .index {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin-top: 30px;
+
+    & h2 {
+      font-size: 1.8em;
+      text-align: center;
+      margin-top: 0;
+    }
+
+    & .ingredients {
+      margin: 30px auto;
+      display: flex;
+
+      & li {
+
+      }
+    }
+    & .delete {
+      position: absolute;
+      top: 4px;
+      right: 4px;
+      cursor: pointer;
+      color: #aaa;
+      font-size: 1.4em;
+    }
+  }
+</style>
