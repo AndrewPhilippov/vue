2c4267c65907443d30e209d530f83789d9114050
a@fandb.pro <a@fandb.pro>
2019-10-07 08:00:32 +0300

    Adding-files

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+      <Navigation />
+    <main>
+      <Carousel />
+    </main>
+  </v-app>
+</template>
+
+<script>
+  import Navigation from '@/components/Navigation'
+  import Carousel from "@/components/Carousel";
+    export default {
+        data() {
+            return {
+            }
+        },
+        components: {
+            Carousel,
+            Navigation
+        }
+    }
+</script>
+<style scoped>
+</style>
