5b6098da18560ae3c2dc0a2e004205e70dd1b76f
a@fandb.pro <a@fandb.pro>
2019-10-08 08:10:18 +0300

    Navigation fixed

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <v-navigation-drawer absolute v-model="sideNav"></v-navigation-drawer>
+
     <v-app-bar color="deep-purple darken-4"
                dark fixed>
+
       <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav">
       </v-app-bar-nav-icon>
 
@@ -11,21 +12,9 @@
       <div class="flex-grow-1"></div>
 
       <v-toolbar-items>
-        <v-btn text>
-          <v-icon icon left>mdi-newspaper</v-icon>
-          Politics
-        </v-btn>
-        <v-btn text>
-          <v-icon icon left>mdi-bank</v-icon>
-          Economics
-        </v-btn>
-        <v-btn text>
-          <v-icon icon left>mdi-book-open-page-variant</v-icon>
-          Culture
-        </v-btn>
-        <v-btn text>
-          <v-icon icon left>mdi-soccer</v-icon>
-          Sports
+        <v-btn text v-for="item in items">
+          <v-icon icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
 
@@ -33,6 +22,39 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app temporary v-model="sideNav">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            News
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Choose your category here
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -45,8 +67,11 @@
                 sideNav: false,
                 items: [
                     {title: 'Dashboard', icon: 'mdi-view-dashboard'},
-                    {title: 'Photos', icon: 'mdi-image'},
-                    {title: 'About', icon: 'mdi-help-box'},
+                    {title: 'Politics', icon: 'mdi-newspaper'},
+                    {title: 'Economics', icon: 'mdi-bank'},
+                    {title: 'Culture', icon: 'mdi-book-open-page-variant'},
+                    {title: 'Sports', icon: 'mdi-soccer'},
+                    {title: 'Health', icon: 'mdi-help-box'},
                 ],
                 right: null,
             }
