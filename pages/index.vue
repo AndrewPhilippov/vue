f67e99d6f38b4f9a39a43013e949af8d3898f070
a@fandb.pro <a@fandb.pro>
2019-10-09 20:33:03 +0300

    Mobile visibility for TopNavbar added

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-layout>
-
-  </v-layout>
+  <v-app>
+    <Navigation />
+    <main>
+      <Carousel />
+    </main>
+  </v-app>
 </template>
 
 <script>
-
-export default {
-  components: {
-  }
-}
+    import Navigation from '@/components/Navigation'
+    import Carousel from "@/components/Carousel";
+    export default {
+        data() {
+            return {
+            }
+        },
+        components: {
+            Carousel,
+            Navigation
+        }
+    }
 </script>
+<style scoped>
+</style>
